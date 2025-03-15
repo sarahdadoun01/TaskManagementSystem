bd4783aef20542d98ee0a72469a59d13bfb35df6
sarahdadoun01 <85398444+sarahdadoun@users.noreply.github.com>
2025-03-15 12:19:25 -0400

    added HomeController.cs

--- api/Controllers/HomeController.cs
+++ api/Controllers/HomeController.cs
@@ -1,2 +1,26 @@
+namespace API.Controllers {
 
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
 
+    [ApiController]
+    [Route("")]
+    public class HomeController: ControllerBase
+    {
+        // http://localhost/
+        [HttpGet]
+        public IActionResult ApiIsWroking()
+        {
+            return Ok(new { Status = "Success", Message = "API is working"});
+        }
+
+        // htpp://localhost/protected
+        [HttpGet("protected")]
+        [Authorize(Policy = "RequireUserRole")]
+        public IActionResult ProtectedEndpoint()
+        {
+            return Ok(new { Status = "Success", Message = "If you are logged, you can see me"});
+        }
+
+    }
+}
