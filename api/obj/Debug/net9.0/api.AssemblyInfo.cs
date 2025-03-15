e396401648871ca96ca884278216dae95b7c141b
sarahdadoun01 <85398444+sarahdadoun@users.noreply.github.com>
2025-03-15 12:55:29 -0400

    added AuthController.cs

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0412025d621a393695f183c2edee89fcc4b72cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd4783aef20542d98ee0a72469a59d13bfb35df6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
