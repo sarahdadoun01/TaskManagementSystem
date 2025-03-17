336271a7201027f36dfae637adcaa1ac2a37e687
sarahdadoun01 <85398444+sarahdadoun@users.noreply.github.com>
2025-03-17 10:10:40 -0400

    app Next.JS/ReactJS

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,20 @@
 # Stage 1 (BUILD)
 FROM node:lts-alpine AS builder
+
+# Inside of our Image container
 WORKDIR /app
-COPY ./app .
+
+COPY ./app/package*.json /app/
+
+RUN npm install
+
+COPY ./app /app
+
+RUN npm run build
 
 # Stage 2 (RUN)
 FROM nginx:alpine
-COPY --from=builder /app /usr/share/nginx/html
+COPY --from=builder /app/dist /usr/share/nginx/html
 COPY default.conf /etc/nginx/conf.d/default.conf
 
 EXPOSE 80
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e396401648871ca96ca884278216dae95b7c141b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b49113c64815b576262f6b4c177f08e112ba6c48")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
